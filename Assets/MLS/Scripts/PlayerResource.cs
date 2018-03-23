de857bb534e2e2bd97cb1042420b3d4b21109da7
idzuna <idzuna>
2018-03-23 16:53:39 +1300

    Whiskey Commit

---
+++ Assets/MLS/Scripts/PlayerResource.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerResource : MonoBehaviour {
+
+	public long StartingResource = 400;
+	private TextMesh _textMesh;
+	private long playerResource = 0;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerResource = StartingResource;
+		_textMesh = GetComponent<TextMesh>();
+		_textMesh.text = playerResource + "";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_textMesh.text = playerResource + "";
+	}
+
+	public void increaseResouce(int increase)
+	{
+		playerResource += increase;
+	}
+
+	public long getResource()
+	{
+		return playerResource;
+	}
+}
---
+++ Assets/MLS/Scripts/Worker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worker : MonoBehaviour
+{
+
+
+	public PlayerResource playerResources;
+
+	public float workRate = 1;
+
+	private DateTime dateDelta;
+
+	// Use this for initialization
+	void Start ()
+	{
+		dateDelta = DateTime.Now;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		DateTime now = DateTime.Now;
+		if (dateDelta.AddSeconds(1/workRate) < now)
+		{
+			dateDelta = now;
+			playerResources.increaseResouce(1);
+		}
+	}
+}
